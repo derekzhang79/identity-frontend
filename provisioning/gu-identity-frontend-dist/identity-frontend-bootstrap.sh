dee06fc6d4fec99f89c889b4927697439b2e98ba
james@jamespamplin.com <james@jamespamplin.com>
2015-11-12 14:24:02 +0000

    Update bootstap and provisioning for artifact locations

--- provisioning/gu-identity-frontend-dist/identity-frontend-bootstrap.sh
+++ provisioning/gu-identity-frontend-dist/identity-frontend-bootstrap.sh
@@ -5,12 +5,11 @@ source set-env.sh
 adduser --home /$apptag --disabled-password --gecos \"\" $apptag
 
 aws s3 cp s3://gu-$apptag-dist/$apptag-upstart.conf /etc/init/$apptag.conf
-aws s3 cp s3://gu-$apptag-dist/$stacktag/$stagetag/$apptag/$apptag-1.0.0-SNAPSHOT.tgz /$apptag/$apptag.tar.gz
-aws s3 cp s3://gu-$apptag-private/$stagetag/$apptag.conf /etc/gu/$apptag.conf
-aws s3 cp s3://gu-$apptag-private/$apptag-cert.json /etc/gu/$apptag-cert.json
+aws s3 cp s3://gu-$apptag-dist/$stacktag/$stagetag/$apptag/$apptag.tgz /$apptag/$apptag.tar.gz
+#aws s3 cp s3://gu-$apptag-private/$stagetag/$apptag.conf /etc/gu/$apptag.conf
+#aws s3 cp s3://gu-$apptag-private/$apptag-cert.json /etc/gu/$apptag-cert.json
 
 tar -xvzf /$apptag/$apptag.tar.gz -C /$apptag
 
 chown -R $apptag /$apptag
-sed -i "s/<APP>/$apptag/g" /etc/init/$apptag.conf
 sed -i "s/<STAGE>/$stagetag/g" /etc/init/$apptag.conf

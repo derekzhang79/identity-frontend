41a5c5b9a4610d468feec59ce3f45465ec8058be
james@jamespamplin.com <james@jamespamplin.com>
2015-11-13 17:00:27 +0000

    Removed unnecessary line

--- provisioning/gu-identity-frontend-dist/identity-frontend-bootstrap.sh
+++ provisioning/gu-identity-frontend-dist/identity-frontend-bootstrap.sh
@@ -7,7 +7,6 @@ adduser --home /$apptag --disabled-password --gecos \"\" $apptag
 aws s3 cp s3://gu-$apptag-dist/$apptag-upstart.conf /etc/init/$apptag.conf
 aws s3 cp s3://gu-$apptag-dist/$stacktag/$stagetag/$apptag/$apptag.tgz /$apptag/$apptag.tar.gz
 #aws s3 cp s3://gu-$apptag-private/$stagetag/$apptag.conf /etc/gu/$apptag.conf
-#aws s3 cp s3://gu-$apptag-private/$apptag-cert.json /etc/gu/$apptag-cert.json
 
 tar -xvzf /$apptag/$apptag.tar.gz -C /$apptag
 

0f44b4242903ce60713b92f5e7c6c95b24643387
james@jamespamplin.com <james@jamespamplin.com>
2015-11-16 11:52:24 +0000

    Set node version on CI

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -1,7 +1,8 @@
 package com.gu.identity.frontend.views.models
 
 
-case class SignInViewModel(title: String = "Sign in to the Guardian", pageTitle: String = "Sign in") extends ViewModel {
+case class SignInViewModel(title: String = "Sign in to the Guardian",
+                           pageTitle: String = "Sign in") extends ViewModel {
   def toMap =
     Map("title" -> title, "pageTitle" -> pageTitle)
 }

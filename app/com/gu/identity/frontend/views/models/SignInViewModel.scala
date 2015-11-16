41ed1b97b8ac984ff8cf5ab0ed6011df81ce9aed
james@jamespamplin.com <james@jamespamplin.com>
2015-11-16 17:53:02 +0000

    Basic sign in page styling, with basic grid impl

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -1,8 +1,14 @@
 package com.gu.identity.frontend.views.models
 
+case class SignInLinksViewModel(socialFacebook: String = "https://oauth.theguardian.com/facebook/signin",
+                                socialGoogle: String = "https://oauth.theguardian.com/google/signin") extends ViewModel {
+  def toMap =
+    Map("socialFacebook" -> socialFacebook, "socialGoogle" -> socialGoogle)
+}
 
 case class SignInViewModel(title: String = "Sign in to the Guardian",
-                           pageTitle: String = "Sign in") extends ViewModel {
+                           pageTitle: String = "Sign in",
+                           links: SignInLinksViewModel = SignInLinksViewModel()) extends ViewModel {
   def toMap =
-    Map("title" -> title, "pageTitle" -> pageTitle)
+    Map("title" -> title, "pageTitle" -> pageTitle, "links" -> links.toMap)
 }

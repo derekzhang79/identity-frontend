ffc53cb753f06e8f49b4e136d56d8a159e69a208
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-02 15:42:46 +0000

    Refactor error message logic.

--- app/com/gu/identity/frontend/views/ViewRenderer.scala
+++ app/com/gu/identity/frontend/views/ViewRenderer.scala
@@ -1,6 +1,6 @@
 package com.gu.identity.frontend.views
 
-import com.gu.identity.frontend.views.models.{LayoutViewModel, SignInViewModel}
+import com.gu.identity.frontend.views.models.{ErrorViewModel, LayoutViewModel, SignInViewModel}
 import jp.co.bizreach.play2handlebars.HBS
 
 /**
@@ -10,7 +10,8 @@ object ViewRenderer {
   def render(view: String, attributes: Map[String, Any] = Map.empty) =
     HBS(view, attributes)
 
-  def renderSignIn(errors: Seq[String]) = {
+  def renderSignIn(errorIds: Seq[String]) = {
+    val errors = errorIds.map(ErrorViewModel.apply)
     val attrs = LayoutViewModel().toMap ++ SignInViewModel(errors = errors).toMap
     render("signin-page", attrs)
   }
--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -8,12 +8,34 @@ case class SignInLinksViewModel(socialFacebook: String = "https://oauth.theguard
     Map("socialFacebook" -> socialFacebook, "socialGoogle" -> socialGoogle)
 }
 
+case class ErrorViewModel(id: String, message: String) extends ViewModel {
+  def toMap =
+    Map("id" -> id, "message" -> message)
+}
+
+object ErrorViewModel {
+
+  def apply(id: String): ErrorViewModel = {
+    ErrorViewModel(id, getErrorMessage(id))
+  }
+
+  val errorMessages = Map(
+    "error-gateway" -> "There was a problem signing in, please try again.",
+    "error-bad-request" -> "Incorrect email or password, please try again."
+  )
+
+  val default: String = "There was an unexpected problem, please try again."
+
+  private def getErrorMessage(id: String) = errorMessages.getOrElse(id, default)
+}
+
+
 case class SignInViewModel(title: String = "Sign in to the Guardian",
                            pageTitle: String = "Sign in",
                            showPrelude: Boolean = false,
-                           errors: Seq[String] = Seq.empty,
+                           errors: Seq[ErrorViewModel] = Seq.empty,
                            links: SignInLinksViewModel = SignInLinksViewModel(),
                            actions: Map[String, String] = Map("signIn" -> routes.SigninAction.signIn().url)) extends ViewModel {
   def toMap =
-    Map("title" -> title, "pageTitle" -> pageTitle, "showPrelude" -> showPrelude, "errors" -> errors, "links" -> links.toMap, "actions" -> actions)
+    Map("title" -> title, "pageTitle" -> pageTitle, "showPrelude" -> showPrelude, "errors" -> errors.map(_.toMap), "links" -> links.toMap, "actions" -> actions)
 }

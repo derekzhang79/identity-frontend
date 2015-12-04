e0d8eab577cf6b19adf16128a15070ac53096b45
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-04 12:23:00 +0000

    Move signin text out to a separate object.

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -10,19 +10,24 @@ case class SignInLinksViewModel(socialFacebook: String = "https://oauth.theguard
     Map("socialFacebook" -> socialFacebook, "socialGoogle" -> socialGoogle)
 }
 
-case class SignInViewModel(title: String = "Sign in to the Guardian",
-                           pageTitle: String = "Sign in",
+case class SignInViewModel(pageTitle: String,
+                           pageText: SignInPageText,
                            showPrelude: Boolean = false,
                            links: SignInLinksViewModel = SignInLinksViewModel()) extends ViewModel {
   def toMap =
-    Map("title" -> title, "pageTitle" -> pageTitle, "showPrelude" -> showPrelude, "links" -> links.toMap)
+    Map("pageTitle" -> pageTitle, "pageText" -> pageText, "showPrelude" -> showPrelude, "links" -> links.toMap)
 }
 
 object SignInViewModel {
   def apply(): SignInViewModel = {
     SignInViewModel(
-      Messages("signin.title"),
-      Messages("signin.pagetitle")
+      Messages("signin.pagetitle"),
+      SignInPageText()
     )
   }
 }
+
+case class SignInPageText (
+                      title: String = Messages("signin.title"),
+                      prelude: String = Messages("signin.prelude")
+                        )

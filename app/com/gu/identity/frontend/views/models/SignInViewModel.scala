bcc5c8423c38c1c9b6355c6c7c2bfc8f9731c121
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-04 11:53:47 +0000

    Include langauge file.

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -1,5 +1,9 @@
 package com.gu.identity.frontend.views.models
 
+import play.api.i18n.Messages
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
 case class SignInLinksViewModel(socialFacebook: String = "https://oauth.theguardian.com/facebook/signin",
                                 socialGoogle: String = "https://oauth.theguardian.com/google/signin") extends ViewModel {
   def toMap =
@@ -12,4 +16,13 @@ case class SignInViewModel(title: String = "Sign in to the Guardian",
                            links: SignInLinksViewModel = SignInLinksViewModel()) extends ViewModel {
   def toMap =
     Map("title" -> title, "pageTitle" -> pageTitle, "showPrelude" -> showPrelude, "links" -> links.toMap)
+}
+
+object SignInViewModel {
+  def apply(): SignInViewModel = {
+    SignInViewModel(
+      Messages("signin.title"),
+      Messages("signin.pagetitle")
+    )
+  }
 }

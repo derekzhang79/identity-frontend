d818a7c9b99c3eb2e51811e04fcc6926672873dc
james@jamespamplin.com <james@jamespamplin.com>
2015-12-04 16:38:35 +0000

    Initial port of omniture code from frontend integrated into main layout

--- app/com/gu/identity/frontend/views/models/LayoutViewModel.scala
+++ app/com/gu/identity/frontend/views/models/LayoutViewModel.scala
@@ -3,18 +3,20 @@ package com.gu.identity.frontend.views.models
 import controllers.routes
 
 
-case class LayoutViewModel(styles: Seq[String]) extends ViewModel {
+case class LayoutViewModel(styles: Seq[String], javascripts: Seq[String]) extends ViewModel {
   def toMap =
-    Map("styles" -> styles)
+    Map("styles" -> styles, "javascripts" -> javascripts)
 }
 
 object LayoutViewModel {
 
   val styleFiles = Seq("bundle.css")
+  val javascriptFiles = Seq("main.bundle.js")
 
   def apply(): LayoutViewModel = {
     val styleUrls = styleFiles.map(routes.Assets.at(_).url)
+    val javascriptUrls = javascriptFiles.map(routes.Assets.at(_).url)
 
-    LayoutViewModel(styleUrls)
+    LayoutViewModel(styleUrls, javascriptUrls)
   }
 }

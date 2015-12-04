a7c6c5a70970fe7e7e755707afd90d15ae59919b
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-04 14:52:45 +0000

    Move text models out of file.

---
+++ app/com/gu/identity/frontend/models/Text.scala
@@ -0,0 +1,33 @@
+package com.gu.identity.frontend.models
+
+import play.api.i18n.Messages
+import play.api.Play.current
+import play.api.i18n.Messages.Implicits._
+
+case class SignInPageText (
+                            title: String = Messages("signin.title"),
+                            pageTitle: String = Messages("signin.pagetitle"),
+                            prelude: String = Messages("signin.prelude"),
+                            preludeMoreInfo: String = Messages("signin.prelude.moreinfo"),
+                            preludeFaq: String = Messages("signin.prelude.faq"),
+                            email: String = Messages("signin.email"),
+                            divideText: String = Messages("signin.dividetext"),
+                            password: String = Messages("signin.password"),
+                            forgottenPassword: String = Messages("signin.forgottenpassword"),
+                            rememberMe: String = Messages("signin.rememberme"),
+                            signIn: String = Messages("signin.signin"),
+                            noAccount: String = Messages("signin.noaccount"),
+                            signUp: String = Messages("signin.signup"),
+                            conditions: String = Messages("signin.conditions"),
+                            termsOfService: String = Messages("signin.termsofservice"),
+                            privacyPolicy: String = Messages("signin.privacypolicy")
+                            )
+
+case class LayoutText (
+                        layoutPageTitle: String = Messages("layout.pagetitle")
+                        )
+
+case class SocialSignInText (
+                              facebook: String = Messages("socialsignin.description", "Facebook"),
+                              google: String = Messages("socialsignin.description", "Google")
+                              )
--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -1,8 +1,6 @@
 package com.gu.identity.frontend.views.models
 
-import play.api.i18n.Messages
-import play.api.Play.current
-import play.api.i18n.Messages.Implicits._
+import com.gu.identity.frontend.models._
 
 case class SignInLinksViewModel(socialFacebook: String = "https://oauth.theguardian.com/facebook/signin",
                                 socialGoogle: String = "https://oauth.theguardian.com/google/signin") extends ViewModel {
@@ -28,31 +26,3 @@ object SignInViewModel {
     )
   }
 }
-
-case class SignInPageText (
-                      title: String = Messages("signin.title"),
-                      pageTitle: String = Messages("signin.pagetitle"),
-                      prelude: String = Messages("signin.prelude"),
-                      preludeMoreInfo: String = Messages("signin.prelude.moreinfo"),
-                      preludeFaq: String = Messages("signin.prelude.faq"),
-                      email: String = Messages("signin.email"),
-                      divideText: String = Messages("signin.dividetext"),
-                      password: String = Messages("signin.password"),
-                      forgottenPassword: String = Messages("signin.forgottenpassword"),
-                      rememberMe: String = Messages("signin.rememberme"),
-                      signIn: String = Messages("signin.signin"),
-                      noAccount: String = Messages("signin.noaccount"),
-                      signUp: String = Messages("signin.signup"),
-                      conditions: String = Messages("signin.conditions"),
-                      termsOfService: String = Messages("signin.termsofservice"),
-                      privacyPolicy: String = Messages("signin.privacypolicy")
-                        )
-
-case class LayoutText (
-                      layoutPageTitle: String = Messages("layout.pagetitle")
-                        )
-
-case class SocialSignInText (
-                            facebook: String = Messages("socialsignin.description", "Facebook"),
-                            google: String = Messages("socialsignin.description", "Google")
-                              )

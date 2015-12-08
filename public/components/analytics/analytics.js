84750ebe0b0b51060dca50324a6ff3281722b21a
james@jamespamplin.com <james@jamespamplin.com>
2015-12-08 18:04:50 +0000

    Log initial a/b test results with Ophan

--- public/components/analytics/analytics.js
+++ public/components/analytics/analytics.js
@@ -1,8 +1,37 @@
 import omniture from './omniture-adapter';
-import { init as initOphan } from './ophan';
+import { init as initOphan, record as recordOphan } from './ophan';
 
-// Log page view events
+// Log initial page view events
 export function logPageView() {
   omniture.go();
-  initOphan();
+
+  // for ophan, this will also log the initial page view:
+  logMVTResults();
+}
+
+const AB_TEST_PREFIX = 'abIdentity';
+
+function getActiveTests() {
+  return {
+    'SignInV2': 'A'
+  };
+}
+
+function getActiveTestsWithLogPrefix() {
+  const tests = getActiveTests();
+  var out = {};
+
+  Object.keys(tests).forEach(key => {
+    out[AB_TEST_PREFIX + key] = tests[key];
+  });
+
+  return out;
+}
+
+function logMVTResults() {
+  const params = {
+    'abTestRegister': getActiveTestsWithLogPrefix()
+  };
+
+  return recordOphan( params );
 }

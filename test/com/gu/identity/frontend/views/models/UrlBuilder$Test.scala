9fc50cd8f0cdf5363614b8620902dab30b39a678
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-09 15:18:24 +0000

    Update tests to reflect changes to params passed in to UrlBuilder apply().

--- test/com/gu/identity/frontend/views/models/UrlBuilder$Test.scala
+++ test/com/gu/identity/frontend/views/models/UrlBuilder$Test.scala
@@ -5,10 +5,10 @@ import org.scalatest.{FlatSpec, Matchers}
 class UrlBuilder$Test extends FlatSpec with Matchers{
 
   it should "create url register plus return url param" in {
-    UrlBuilder("register", "redirectUrl", Some("www.test.com")) should be ("register/?redirectUrl=www.test.com")
+    UrlBuilder("register", Some(("redirectUrl", "www.test.com"))) should be ("register/?redirectUrl=www.test.com")
   }
 
   it should "create url register with no params" in {
-    UrlBuilder("register", "none", None) should be ("register")
+    UrlBuilder("register", None) should be ("register")
   }
 }

0216a5f08fd9af2b107ad7759d607d98291f2d3f
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-15 14:42:19 +0000

    Added map and case class for form.

--- app/com/gu/identity/frontend/controllers/RegisterAction.scala
+++ app/com/gu/identity/frontend/controllers/RegisterAction.scala
@@ -2,11 +2,43 @@ package com.gu.identity.frontend.controllers
 
 import com.gu.identity.frontend.logging.Logging
 import com.gu.identity.frontend.services.IdentityService
+import play.api.data.{Mapping, Form}
+import play.api.data.Forms._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
 
 class RegisterAction(identityService: IdentityService, val messagesApi: MessagesApi) extends Controller with Logging with I18nSupport {
+
+  case class RegisterRequest(
+                            firstName: String,
+                            lastName: String,
+                            email: String,
+                            username: String,
+                            password: String,
+                            receiveGnmMarketing: Boolean,
+                            receive3rdPartyMarketing: Boolean
+                              )
+
+  private val username: Mapping[String] = text.verifying(
+    "error.username", name => name.matches("[A-z0-9]+") && name.length > 5 && name.length < 21
+  )
+
+  private val password: Mapping[String] = text.verifying(
+    "error.password", pwd => pwd.length > 5 && pwd.length < 21
+  )
+
+  val registerForm = Form(
+    mapping(
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
+      "email" -> email,
+      "username" -> username,
+      "password" -> password,
+      "receiveGnmMarketing" -> boolean,
+      "receive3rdPartyMarketing" -> boolean
+    )(RegisterRequest.apply)(RegisterRequest.unapply)
+  )
 
   def register = Action.async { implicit request => ???
   }

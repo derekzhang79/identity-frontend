8def49a76b04c4f4d78f84e2bc6aafc792ca70f0
james@jamespamplin.com <james@jamespamplin.com>
2015-12-16 16:27:02 +0000

    Removed trailing slash from links

--- test/com/gu/identity/frontend/views/models/UrlBuilder$Test.scala
+++ test/com/gu/identity/frontend/views/models/UrlBuilder$Test.scala
@@ -5,7 +5,7 @@ import org.scalatest.{FlatSpec, Matchers}
 class UrlBuilder$Test extends FlatSpec with Matchers{
 
   it should "create url register with return url" in {
-    UrlBuilder("register", Seq(("returnUrl", "www.test.com"))) should be ("register/?returnUrl=www.test.com")
+    UrlBuilder("register", Seq(("returnUrl", "www.test.com"))) should be ("register?returnUrl=www.test.com")
   }
 
   it should "create url register with no params" in {
@@ -13,6 +13,6 @@ class UrlBuilder$Test extends FlatSpec with Matchers{
   }
 
   it should "create a register url with 'returnUrl=www.test.com' and ignore any None objects" in {
-    UrlBuilder("register", Seq(("returnUrl", "www.test.com"))) should be ("register/?returnUrl=www.test.com")
+    UrlBuilder("register", Seq(("returnUrl", "www.test.com"))) should be ("register?returnUrl=www.test.com")
   }
 }

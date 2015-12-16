2dd5e7366687b7d411cde5b76286f754a61c6e04
Susie Coleman <susie.coleman@guardian.co.uk>
2015-12-16 11:59:26 +0000

    Move out error view model to decouple it from signin view model.

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -19,27 +19,6 @@ object SignInLinksViewModel {
   }
 }
 
-case class ErrorViewModel(id: String, message: String) extends ViewModel {
-  def toMap(implicit messages: Messages) =
-    Map("id" -> id, "message" -> message)
-}
-
-object ErrorViewModel {
-
-  def apply(id: String): ErrorViewModel = {
-    ErrorViewModel(id, getErrorMessage(id))
-  }
-
-  val errorMessages = Map(
-    "error-gateway" -> "There was a problem signing in, please try again.",
-    "error-bad-request" -> "Incorrect email or password, please try again."
-  )
-
-  val default: String = "There was an unexpected problem, please try again."
-
-  private def getErrorMessage(id: String) = errorMessages.getOrElse(id, default)
-}
-
 case class SignInViewModel(showPrelude: Boolean = false,
                            errors: Seq[ErrorViewModel] = Seq.empty,
                            email: String = "",

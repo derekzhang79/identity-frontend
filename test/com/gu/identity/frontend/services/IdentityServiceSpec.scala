44ea6634665bb9db55b7328b246caa5262facbd4
Susie Coleman <susie.coleman@guardian.co.uk>
2016-01-08 12:24:00 +0000

    Delete empty test class.

--- test/com/gu/identity/frontend/services/IdentityServiceSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.gu.identity.frontend.services
-
-import org.scalatest.mock.MockitoSugar
-import org.scalatestplus.play.PlaySpec
-
-class IdentityServiceSpec extends PlaySpec with MockitoSugar {
-
-
-}

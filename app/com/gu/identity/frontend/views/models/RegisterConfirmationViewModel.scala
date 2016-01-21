876c74a47e5f2ee653afc39d870fbc10696e0dc7
Susie Coleman <susie.coleman@guardian.co.uk>
2016-01-21 11:51:40 +0000

    Add styling and move url building out of the view.

--- app/com/gu/identity/frontend/views/models/RegisterConfirmationViewModel.scala
+++ app/com/gu/identity/frontend/views/models/RegisterConfirmationViewModel.scala
@@ -1,11 +1,16 @@
 package com.gu.identity.frontend.views.models
 
 import com.gu.identity.frontend.configuration.Configuration
+import com.gu.identity.frontend.models.UrlBuilder
 import play.api.i18n.Messages
 
 case class RegisterConfirmationViewModel private(
     layout: LayoutViewModel,
     returnUrl: String,
+    faqUrl: String = "http://www.theguardian.com/help/identity-faq",
+    emailUserHelpUrl: String = "mailto:userhelp@theguardian.com?subject=Account help",
+    resetPasswordUrl: String,
+    signOutUrl: String,
     resources: Seq[PageResource with Product],
     indirectResources: Seq[PageResource with Product])
   extends ViewModel
@@ -14,10 +19,13 @@ case class RegisterConfirmationViewModel private(
 object RegisterConfirmationViewModel {
   def apply(configuration: Configuration, returnUrl: Option[String])(implicit messages: Messages): RegisterConfirmationViewModel = {
     val layout = LayoutViewModel(configuration, None)
+    val urlParams = Seq(("returnUrl" -> returnUrl.getOrElse("http://www.theguardian.com")))
 
     RegisterConfirmationViewModel(
       layout = layout,
       returnUrl = returnUrl.getOrElse("http://www.theguardian.com"),
+      resetPasswordUrl = UrlBuilder("https://profile.theguardian.com/reset",urlParams),
+      signOutUrl = UrlBuilder("https://profile.theguardian.com/signout",urlParams),
       resources = layout.resources,
       indirectResources = layout.indirectResources
     )

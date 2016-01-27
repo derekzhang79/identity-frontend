5e7cbae43d71437a3a78a4087cd2df724722508c
Mario Galic <mario.galic@guardian.co.uk>
2016-01-27 17:35:50 +0000

    Update register page for RegisterV2

--- functional-tests/src/test/scala/pages/RegisterConfirm.scala
+++ functional-tests/src/test/scala/pages/RegisterConfirm.scala
@@ -3,11 +3,11 @@ package test.pages
 import test.util.{LoadablePage, Browser, Config}
 
 class RegisterConfirm extends LoadablePage with Browser {
-  val url = s"""${Config.baseUrl}/register/confirm?returnUrl=${Config.baseUrl}/register"""
+  val url = s"${Config.baseUrl}/register/confirm?returnUrl=${Config.baseUrl}/register"
 
   def hasLoaded(): Boolean = pageHasElement(confirmRegistrationButton)
 
   def confirmRegistration() = clickOn(confirmRegistrationButton)
 
-  private lazy val confirmRegistrationButton = className(s"submit-input")
+  private lazy val confirmRegistrationButton = className("register-confirm__complete-registration-button")
 }

43c2676c405ada93d3101283baed1547a64db1a9
james@jamespamplin.com <james@jamespamplin.com>
2016-01-28 17:35:53 +0000

    Init persist state logic for register form

---
+++ public/components/register-form/register-form.js
@@ -0,0 +1,122 @@
+
+import { getElementById, sessionStorage } from '../browser/browser';
+
+const STORAGE_KEY = 'gu_id_register_state';
+
+
+class RegisterFormFields {
+  constructor(firstNameField, lastNameField, emailField, usernameField) {
+    this.firstName = firstNameField;
+    this.lastName = lastNameField;
+    this.email = emailField;
+    this.username = usernameField;
+  }
+
+  setValues( { firstName, lastName, email, username } = {} ) {
+    this.firstName.setValue( firstName );
+    this.lastName.setValue( lastName );
+    this.email.setValue( email );
+    this.username.setValue( username );
+  }
+
+  toJSON() {
+    // TODO use _.mapValues
+
+    return {
+      firstName: this.firstName.value(),
+      lastName: this.lastName.value(),
+      email: this.email.value(),
+      username: this.username.value()
+    };
+  }
+}
+
+
+class RegisterFormModel {
+  constructor( formElement, firstNameField, lastNameField, emailField, usernameField ) {
+    this.formElement = formElement;
+
+    this.fields = new RegisterFormFields( firstNameField, lastNameField, emailField, usernameField );
+
+    this.addFormListeners();
+  }
+
+  addFormListeners() {
+    this.formElement.on( 'submit', this.formSubmitted.bind( this ) );
+  }
+
+  loadState() {
+    this.state = RegisterFormState.fromStorage();
+    this.fields.setValues( this.state );
+  }
+
+  saveState() {
+    this.state = RegisterFormState.fromForm( this );
+    this.state.save();
+  }
+
+  formSubmitted() {
+    this.saveState();
+  }
+
+  static fromDocument() {
+    const form = getElementById( 'register_form' );
+    const firstNameField = getElementById( 'register_field_firstname' );
+    const lastNameField = getElementById( 'register_field_lastname' );
+    const emailField = getElementById( 'register_field_email' );
+    const usernameField = getElementById( 'register_field_username' );
+
+    if ( form && firstNameField && lastNameField && emailField && usernameField ) {
+      return new RegisterFormModel( form, firstNameField, lastNameField, emailField, usernameField );
+    }
+  }
+}
+
+
+class RegisterFormState {
+  constructor( firstName = "", lastName = "", email = "", username = "" ) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.username = username;
+  }
+
+  save() {
+    sessionStorage.setJSON( STORAGE_KEY, this );
+  }
+
+  /**
+   * @return {RegisterFormState}
+   */
+  static fromObject( { firstName, lastName, email, username } = {} ) {
+    return new RegisterFormState( firstName, lastName, email, username );
+  }
+
+  /**
+   * @return {RegisterFormState}
+   */
+  static fromStorage() {
+    const existingState = sessionStorage.getJSON( STORAGE_KEY );
+
+    return RegisterFormState.fromObject( existingState )
+  }
+
+  /**
+   * @param {RegisterFormModel} form
+   * @return {RegisterFormState}
+   */
+  static fromForm( form ) {
+    return RegisterFormState.fromObject( form.fields.toJSON() );
+  }
+}
+
+
+export function init() {
+  const form = RegisterFormModel.fromDocument();
+
+  if ( form ) {
+    form.loadState();
+  }
+
+  return form;
+}

56a9568af0481520580a9531c6f9de7032080790
Susie Coleman <susie.coleman@guardian.co.uk>
2016-01-29 14:25:54 +0000

    Rewrite how SignInViewModel is constructed.

--- app/com/gu/identity/frontend/views/models/SignInViewModel.scala
+++ app/com/gu/identity/frontend/views/models/SignInViewModel.scala
@@ -25,8 +25,7 @@ case class SignInViewModel private(
     registerUrl: String = "",
     forgotPasswordUrl: String = "",
 
-    recaptchaEnabled: Boolean,
-    recaptchaSiteKey: String,
+    recaptchaModel: Option[GoogleRecaptchaViewModel],
 
     actions: Map[String, String] = Map("signIn" -> routes.SigninAction.signIn().url),
     resources: Seq[PageResource with Product],
@@ -37,29 +36,12 @@ case class SignInViewModel private(
 
 object SignInViewModel {
   def apply(configuration: Configuration, activeTests: Iterable[(MultiVariantTest, MultiVariantTestVariant)], csrfToken: Option[CSRFToken], errors: Seq[ErrorViewModel], returnUrl: ReturnUrl, skipConfirmation: Option[Boolean])(implicit messages: Messages): SignInViewModel = {
+
     val layout = LayoutViewModel(configuration, activeTests)
+    val recaptchaModel : Option[GoogleRecaptchaViewModel] =
+      getRecaptchaModel(configuration, isError = !errors.isEmpty, recaptchaEnabled = configuration.recaptchaEnabled)
 
-    if(errors.isEmpty || (!configuration.recaptchaEnabled)) {
-      val resources = layout.resources
-      createSignInViewModel(layout, configuration, returnUrl, skipConfirmation, errors, csrfToken, resources)
-    } else {
-      val recaptchaResources = GoogleRecaptchaResources(configuration)
-      val resources = layout.resources ++ recaptchaResources.resources
-      val recaptchaSiteKey = Some(recaptchaResources.googleSiteKey)
-      createSignInViewModel(layout, configuration, returnUrl, skipConfirmation, errors, csrfToken, resources, recaptchaEnabled = true, recaptchaSiteKey)
-    }
-  }
-
-  private def createSignInViewModel(
-       layout: LayoutViewModel,
-       configuration: Configuration,
-       returnUrl: ReturnUrl,
-       skipConfirmation: Option[Boolean],
-       errors: Seq[ErrorViewModel],
-       csrfToken: Option[CSRFToken],
-       resources: Seq[PageResource with Product],
-       recaptchaEnabled: Boolean = false,
-       recaptchaSiteKey: Option[String] = None)(implicit messages: Messages): SignInViewModel = {
+    val resources = getResources(layout, recaptchaModel)
 
     SignInViewModel(
       layout = layout,
@@ -78,16 +60,30 @@ object SignInViewModel {
       registerUrl = UrlBuilder(routes.Application.register(), returnUrl, skipConfirmation),
       forgotPasswordUrl = UrlBuilder("/reset", returnUrl, skipConfirmation),
 
-      recaptchaEnabled = recaptchaEnabled,
-      recaptchaSiteKey = recaptchaSiteKey.getOrElse(""),
+      recaptchaModel = recaptchaModel,
 
       resources = resources,
       indirectResources = layout.indirectResources
     )
   }
+
+  private def getRecaptchaModel(configuration: Configuration, isError: Boolean, recaptchaEnabled: Boolean): Option[GoogleRecaptchaViewModel] = {
+    if(isError && recaptchaEnabled) {
+      Some(GoogleRecaptchaViewModel(configuration))
+    } else {
+      None
+    }
+  }
+  
+  private def getResources(layout: LayoutViewModel, recaptchaViewModel: Option[GoogleRecaptchaViewModel]): Seq[PageResource with Product] ={
+    recaptchaViewModel match {
+      case Some(model) =>  layout.resources ++ model.resources
+      case None => layout.resources
+    }
+  }
 }
 
-case class GoogleRecaptchaResources(
+case class GoogleRecaptchaViewModel(
    resources: Seq[PageResource with Product] = Seq(
      JavascriptResource("https://www.google.com/recaptcha/api.js", "https://www.google.com", isInHead = true),
      IndirectlyLoadedExternalScriptResources("https://www.gstatic.com"),
@@ -97,10 +93,9 @@ case class GoogleRecaptchaResources(
    googleSiteKey: String
    )
 
-object GoogleRecaptchaResources {
-
-  def apply(configuration: Configuration): GoogleRecaptchaResources = {
-    GoogleRecaptchaResources(
+object GoogleRecaptchaViewModel {
+  def apply(configuration: Configuration): GoogleRecaptchaViewModel = {
+    GoogleRecaptchaViewModel(
       googleSiteKey = configuration.googleRecaptchaSiteKey
     )
   }

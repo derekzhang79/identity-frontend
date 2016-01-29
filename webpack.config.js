0fe3ba486b501dc61edbec27f6b237df47681652
james@jamespamplin.com <james@jamespamplin.com>
2016-01-29 11:30:40 +0000

    Don't mangle JS output for better debugging

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,31 @@
-var webpack = require('webpack');
-var path = require('path');
+/*eslint-env node*/
+
+var webpack = require( 'webpack' );
+var path = require( 'path' );
 
 module.exports = {
   entry: {
     main: './public/main'
   },
   output: {
-    path: path.resolve(__dirname, 'target/web/build/'),
-    publicPath: "/static/",
-    filename: "[name].bundle.js"
+    path: path.resolve( __dirname, 'target/web/build/' ),
+    publicPath: '/static/',
+    filename: '[name].bundle.js'
   },
-  devtool: "#source-map",
+  devtool: '#source-map',
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel?presets[]=es2015']
+        loaders: [ 'babel?presets[]=es2015' ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin( {
+      mangle: false
+    } )
+  ]
 };

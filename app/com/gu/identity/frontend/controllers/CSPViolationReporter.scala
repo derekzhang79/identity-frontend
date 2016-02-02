d762fd66f7e89dbe02a0e07fbba39c4fcb1734b7
Susie Coleman <susie.coleman@guardian.co.uk>
2016-02-02 17:18:20 +0000

    Log content security violations.

--- app/com/gu/identity/frontend/controllers/CSPViolationReporter.scala
+++ app/com/gu/identity/frontend/controllers/CSPViolationReporter.scala
@@ -1,14 +1,21 @@
 package com.gu.identity.frontend.controllers
 
+import com.gu.identity.frontend.logging.Logging
 import play.api.mvc.Action
 import play.api.mvc.Results._
+import play.api.mvc.BodyParsers.parse
 
 
-class CSPViolationReporter {
+class CSPViolationReporter extends Logging{
 
-  def cspReport() = Action{ request =>
-    println(request.body)
-    Ok("Reporting Error")
+  def cspReport() = Action(parse.tolerantText) { implicit request =>
+
+    val userAgent = request.headers.get("User-Agent")
+    val report = request.body
+
+    logger.error(s"Content Security Violation Error: ${report}.\nUser Agent: ${userAgent}")
+
+    Ok("Content Security Policy Violation Logged.")
   }
 
 }

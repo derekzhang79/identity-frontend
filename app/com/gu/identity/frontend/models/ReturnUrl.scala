1b8a4fa2a88f090cd3354b9774056b513de0df3b
Susie Coleman <susie.coleman@guardian.co.uk>
2016-02-04 10:04:50 +0000

    Do not allow return url to be /signin /register /register/confirm

--- app/com/gu/identity/frontend/models/ReturnUrl.scala
+++ app/com/gu/identity/frontend/models/ReturnUrl.scala
@@ -9,11 +9,13 @@ object ReturnUrl {
 
   val default = ReturnUrl("http://www.theguardian.com")
   val domains = List(".theguardian.com", ".code.dev-theguardian.com", ".thegulocal.com")
+  val invalidUrlPaths = List("/signin", "/register", "/register/confirm")
 
   def apply(returnUrl: Option[String], referer: Option[String]): ReturnUrl = {
     returnUrl.map(ReturnUrl(_))
       .orElse(referer.map(ReturnUrl(_)))
       .filter(validDomain(_))
+      .filter(validUrlPath(_))
       .getOrElse(default)
   }
 
@@ -22,6 +24,12 @@ object ReturnUrl {
     domains.exists(s".$hostname".endsWith(_))
   }
 
+  def validUrlPath(returnUrl: ReturnUrl): Boolean = {
+    val urlPath = path(returnUrl)
+    !invalidUrlPaths.contains(urlPath)
+  }
+
   def host(returnUrl: ReturnUrl): String = Try(new URI(returnUrl.url)).map(_.getHost).getOrElse("")
 
+  def path(returnUrl: ReturnUrl): String = Try(new URI(returnUrl.url)).map(_.getPath).getOrElse("")
 }

bac17fc51423041fb20075f707a5bce16182f960
Susie Coleman <susie.coleman@guardian.co.uk>
2016-02-10 16:41:38 +0000

    Create and use specific public/private key pair for testing authentication.

--- test/com/gu/identity/frontend/authentication/AuthenticationServiceSpec.scala
+++ test/com/gu/identity/frontend/authentication/AuthenticationServiceSpec.scala
@@ -1,15 +1,20 @@
 package com.gu.identity.frontend.authentication
 
-import com.gu.identity.cookie.{PreProductionKeys, IdentityCookieDecoder}
+import com.gu.identity.cookie.{IdentityKeys, IdentityCookieDecoder}
+import com.gu.identity.frontend.configuration.Configuration
 import org.scalatestplus.play.PlaySpec
 import play.api.mvc.Cookie
 import play.api.test.FakeRequest
 
-class AuthenticationServiceTest extends PlaySpec {
+class AuthenticationServiceSpec extends PlaySpec {
 
-  val cookieDecoder = new IdentityCookieDecoder(new PreProductionKeys())
+  val testKeys = new IdentityKeys(Configuration.testConfiguration.identityCookiePublicKey)
+
+  val cookieDecoder = new IdentityCookieDecoder(testKeys)
 
   val autheticationService = new AuthenticationService(cookieDecoder)
+
+
 
   def requestWithCookie(value: String, name: String = "SC_GU_U") = {
     val cookie = Cookie(name = name, value = value)
@@ -19,10 +24,10 @@ class AuthenticationServiceTest extends PlaySpec {
   "AuthenticatedUserFor" should {
 
     "decode valid cookie" in {
-      val validRequestCookieData = "WyIyMTgzOTIyMSIsMTQ2MjgxNjE4NTg3NV0.MCwCFC8sibZouZgjsEKqE8EFJX81_2g1AhRziWMAzxc9oNmN0SYlHG6vVs1PgA"
+      val validRequestCookieData = "WyIxMDAwMDgxMSIsMTQ2Mjg5MjgyNDYxMV0.MCwCFG_PdoPk2PpSO5KoXbRLWJ0BvuqhAhRFIt1mlDcO2SN1Y6X7ktSs_oRJJw"
       val validCookieRequest = requestWithCookie(value = validRequestCookieData)
       val response = autheticationService.authenticatedUserFor(validCookieRequest)
-      val expectedUser = AuthenticatedUser("21839221")
+      val expectedUser = AuthenticatedUser("10000811")
 
       response mustEqual Some(expectedUser)
     }

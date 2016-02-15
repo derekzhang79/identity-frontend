e56901162d2717f8b5a064b5a73e742e3639ed19
james@jamespamplin.com <james@jamespamplin.com>
2016-02-15 11:50:19 +0000

    Moved Sign in form to own Component

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ import { isSupported as isBrowserSupported } from './components/browser/browser'
 
 import { logPageView } from './components/analytics/analytics';
 
-import { init as initSigninBindings } from './components/signin/signin';
+import { init as initSigninBindings } from './components/signin-form/signin-form';
 import { init as initSigninTestB } from './components/signin-b/signin-b';
 
 import { init as initRegisterBindings } from './components/register-form/register-form';

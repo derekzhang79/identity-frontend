47f6c243cdb10eb54b52857f0e42cb7b474f0e2e
Susie Coleman <susie.coleman@guardian.co.uk>
2016-02-18 15:26:07 +0000

    Add additional text needed for additional Ts and Cs.

--- app/com/gu/identity/frontend/models/text/TermsText.scala
+++ app/com/gu/identity/frontend/models/text/TermsText.scala
@@ -7,11 +7,33 @@ case class TermsText private(
      termsOfServiceText: String,
      privacyPolicyText: String)
 
+case class TeachersTermsText private(
+    basicTermsText: TermsText = TermsText(),
+    conditionsText: String)
+
+case class JobsTermsText private(
+    basicTermsText: TermsText = TermsText(),
+    conditionsText: String)
+
 object TermsText {
   def apply()(implicit messages: Messages): TermsText =
     TermsText(
       conditionsText = messages("terms.conditions"),
       termsOfServiceText = messages("terms.termsOfService"),
       privacyPolicyText = messages("terms.privacyPolicy")
+    )
+}
+
+object TeachersTermsText {
+  def apply()(implicit messages: Messages): TeachersTermsText =
+    TeachersTermsText(
+      conditionsText = messages("terms.teachersConditions")
+    )
+}
+
+object JobsTermsText {
+  def apply()(implicit messages: Messages): JobsTermsText =
+    JobsTermsText(
+      conditionsText = messages("terms.jobsConditions")
     )
 }

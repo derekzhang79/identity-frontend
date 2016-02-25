a67ff5b516f54fbfc03fb7e1a43dab71e02f97c6
Susie Coleman <susie.coleman@guardian.co.uk>
2016-02-25 18:04:20 +0000

    Add check for group membership to ts and cs flow.

--- app/com/gu/identity/frontend/controllers/ThirdPartyTsAndCs.scala
+++ app/com/gu/identity/frontend/controllers/ThirdPartyTsAndCs.scala
@@ -3,7 +3,7 @@ package com.gu.identity.frontend.controllers
 import com.gu.identity.cookie.IdentityCookieDecoder
 import com.gu.identity.frontend.authentication.{CookieName, AuthenticationService}
 import com.gu.identity.frontend.models.GroupCodes
-import com.gu.identity.frontend.services.IdentityService
+import com.gu.identity.frontend.services.{ServiceError, IdentityService}
 import com.gu.identity.service.client.models.User
 import play.api.mvc.Security.AuthenticatedBuilder
 import play.api.mvc._
@@ -25,7 +25,10 @@ class ThirdPartyTsAndCs(identityService: IdentityService, identityCookieDecoder:
       val sc_gu_uCookie = getSC_GU_UCookie(request.cookies)
 
       (isValidGroup, sc_gu_uCookie) match {
-        case(true, Some(cookie)) => renderAgree()
+        case(true, Some(cookie)) => {
+          checkForGroupMembership(group, cookie)
+          Future(Ok("Welcome to 3rd party ts and cs"))
+        }
         case(false, _) => {
           logger.info(s"Received invalid group code $group")
           Future(BadRequest)
@@ -38,20 +41,24 @@ class ThirdPartyTsAndCs(identityService: IdentityService, identityCookieDecoder:
     }
   }
 
-  def renderAgree() = {
-    Future.successful(Ok("Some Ts and Cs"))
+  def checkForGroupMembership(group: String, cookie: Cookie): Future[Either[Seq[ServiceError], Boolean]] = {
+    identityService.getUser(cookie).map{
+      case Right(user) => {
+        Right(isUserInGroup(user, group))
+      }
+      case Left(errors) => {
+        logger.info("Request did not have a SC_GU_U cookie")
+        Left(errors)
+      }
+    }
   }
-
-  def getUser(cookie: Cookie) = {
-    identityService.getUser(cookie)
-  }
-
-  def getSC_GU_UCookie(cookies: Cookies): Option[Cookie] = cookies.get(CookieName.SC_GU_U.toString)
 
   def isUserInGroup(user: User, group: String):Boolean = {
     val usersGroups = user.userGroups
     usersGroups.map(_.packageCode == group).contains(true)
   }
+
+  def getSC_GU_UCookie(cookies: Cookies): Option[Cookie] = cookies.get(CookieName.SC_GU_U.toString)
 
   def isValidGroupCode(group: String): Boolean = {
     GroupCodes.isOrderType(group)
--- test/com/gu/identity/frontend/controllers/ThirdPartyTsAndCsSpec.scala
+++ test/com/gu/identity/frontend/controllers/ThirdPartyTsAndCsSpec.scala
@@ -1,21 +1,32 @@
 package com.gu.identity.frontend.controllers
 
+import akka.util.Timeout
 import com.gu.identity.cookie.IdentityCookieDecoder
-import com.gu.identity.frontend.services.IdentityService
+import com.gu.identity.frontend.services.{ServiceGatewayError, IdentityService}
 import com.gu.identity.service.client.models.{UserGroup, User}
+import org.mockito.Matchers.{any => argAny}
 import org.scalatest.mock.MockitoSugar
 import org.scalatestplus.play.PlaySpec
+import org.mockito.Mockito._
+import play.api.mvc.Cookie
+import scala.concurrent.duration._
+
+import scala.concurrent.{Await, ExecutionContext, Future}
 
 class ThirdPartyTsAndCsSpec extends PlaySpec with MockitoSugar{
 
-  val mockIdentityService = mock[IdentityService]
-  val mockIdentityCookieDecoder = mock[IdentityCookieDecoder]
+  trait WithControllerMockedDependencies {
+    val mockIdentityService = mock[IdentityService]
+    val mockIdentityCookieDecoder = mock[IdentityCookieDecoder]
 
-  val thirdPartyTsAndCsController = new ThirdPartyTsAndCs(mockIdentityService, mockIdentityCookieDecoder)
+    val thirdPartyTsAndCsController = new ThirdPartyTsAndCs(mockIdentityService, mockIdentityCookieDecoder)
+  }
+
+
 
   "ThirdPartyTsAndCs" should {
 
-    "return true if user is in group specified" in {
+    "return true if user is in group specified" in new WithControllerMockedDependencies {
       val groupCode = "ABC"
       val userGroup = UserGroup(groupCode, "Group/ABC")
       val userGroups = List(userGroup)
@@ -24,7 +35,7 @@ class ThirdPartyTsAndCsSpec extends PlaySpec with MockitoSugar{
       thirdPartyTsAndCsController.isUserInGroup(user, groupCode) mustEqual true
     }
 
-    "return false if user is not in the group specified" in {
+    "return false if user is not in the group specified" in  new WithControllerMockedDependencies {
       val groupCode = "123"
       val userGroup = UserGroup("ABC", "Group/ABC")
       val userGroups = List(userGroup)
@@ -32,6 +43,74 @@ class ThirdPartyTsAndCsSpec extends PlaySpec with MockitoSugar{
 
       thirdPartyTsAndCsController.isUserInGroup(user, groupCode) mustEqual false
     }
-  }
 
+    "return a future of true when the user is in already in the group specified" in new WithControllerMockedDependencies {
+      val groupCode = "ABC"
+      val userGroup = UserGroup(groupCode, "Group/ABC")
+      val userGroups = List(userGroup)
+      val user = User(userGroups = userGroups)
+
+      val timeout = Timeout(5 seconds)
+
+      val cookie = Cookie("Name", "Value")
+
+      when(mockIdentityService.getUser(argAny[Cookie])(argAny[ExecutionContext]))
+        .thenReturn {
+          Future.successful{
+            Right(user)
+          }
+        }
+
+      val future = thirdPartyTsAndCsController.checkForGroupMembership(groupCode, cookie)
+      val result = Await.result(future, timeout.duration)
+
+      result mustEqual Right(true)
+    }
+
+    "return a future of false when the user is not in the group specified" in new WithControllerMockedDependencies {
+      val groupCode = "ABC"
+      val userGroup = UserGroup("123", "Group/ABC")
+      val userGroups = List(userGroup)
+      val user = User(userGroups = userGroups)
+
+      val timeout = Timeout(5 seconds)
+
+      val cookie = Cookie("Name", "Value")
+
+      when(mockIdentityService.getUser(argAny[Cookie])(argAny[ExecutionContext]))
+        .thenReturn {
+          Future.successful{
+            Right(user)
+          }
+        }
+
+      val future = thirdPartyTsAndCsController.checkForGroupMembership(groupCode, cookie)
+      val result = Await.result(future, timeout.duration)
+
+      result mustEqual Right(false)
+    }
+
+    "return a future of sequence of service errors if it was not possible to check if the user is in the group" in new WithControllerMockedDependencies {
+      val groupCode = "ABC"
+      val userGroup = UserGroup(groupCode, "Group/ABC")
+      val userGroups = List(userGroup)
+      val user = User(userGroups = userGroups)
+
+      val timeout = Timeout(5 seconds)
+
+      val cookie = Cookie("Name", "Value")
+
+      when(mockIdentityService.getUser(argAny[Cookie])(argAny[ExecutionContext]))
+        .thenReturn {
+          Future.successful{
+            Left(Seq(ServiceGatewayError("Unexpected 500 error")))
+          }
+        }
+
+      val future = thirdPartyTsAndCsController.checkForGroupMembership(groupCode, cookie)
+      val result = Await.result(future, timeout.duration)
+
+      result mustEqual Left(Seq(ServiceGatewayError("Unexpected 500 error")))
+    }
+  }
 }

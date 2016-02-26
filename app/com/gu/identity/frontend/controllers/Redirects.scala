c4be7aed97072998be168ada65945ffa8eca0c8b
james@jamespamplin.com <james@jamespamplin.com>
2016-02-26 18:06:23 +0000

    Capture legacy routes and redirect
    - New Redirects controller for all redirects
    - Legacy routes log request uri and referrer, redirect with permanent
      redirect code

---
+++ app/com/gu/identity/frontend/controllers/Redirects.scala
@@ -0,0 +1,38 @@
+package com.gu.identity.frontend.controllers
+
+import com.gu.identity.frontend.logging.Logging
+import play.api.mvc.{Call, Action, Controller}
+
+
+final class Redirects extends Controller with Logging {
+
+  lazy val signInRoute: Call = routes.Application.signIn()
+  lazy val registerRoute: Call = routes.Application.register()
+
+
+  def indexRedirect =
+    redirectToRoute(signInRoute)
+
+
+  def signInPageTrailingSlash =
+    redirectToRoute(signInRoute, isLegacy = true)
+
+
+  def registerPageTrailingSlash =
+    redirectToRoute(registerRoute, isLegacy = true)
+
+
+  private def redirectToRoute(call: Call, isLegacy: Boolean = false) = Action { request =>
+    if (isLegacy) {
+      val referrer = request.headers.get(REFERER).getOrElse("_unknown_")
+
+      logger.info(s"Legacy route called: ${request.uri} from $referrer")
+
+      MovedPermanently(call.url)
+
+    } else {
+      SeeOther(call.url)
+    }
+  }
+
+}

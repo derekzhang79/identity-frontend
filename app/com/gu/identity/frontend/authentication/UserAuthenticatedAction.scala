77ac94a4c854233113642146de30c18e0d5eaff2
Susie Coleman <susie.coleman@guardian.co.uk>
2016-03-03 10:44:43 +0000

    Remove unneeded code.

--- app/com/gu/identity/frontend/authentication/UserAuthenticatedAction.scala
+++ app/com/gu/identity/frontend/authentication/UserAuthenticatedAction.scala
@@ -37,23 +37,3 @@ object UserAuthenticatedActionBuilder extends Logging{
   def getSC_GU_UCookie(cookies: Cookies): Option[Cookie] = cookies.get(CookieName.SC_GU_U.toString)
 }
 
-//object UserAuthenticatedAction extends ActionRefiner[Request, UserAuthenticatedRequest] with ActionBuilder[UserAuthenticatedRequest] with Logging {
-//
-//  override protected def refine[A](request: Request[A], identityCookieDecoder: IdentityCookieDecoder): Future[Either[Result, UserAuthenticatedRequest[A]]] = Future {
-//
-//    AuthenticationService.authenticatedUserFor(request, identityCookieDecoder.getUserDataForScGuU) match {
-//      case Some(authenticatedUser) => {
-//        getSC_GU_UCookie(request.cookies) match {
-//          case Some(cookie) => Right(new UserAuthenticatedRequest[A](cookie, request))
-//          case _ => {
-//            logger.error("Cookie not found on successfully authenticated request.")
-//            Left(SeeOther("/signin"))
-//          }
-//        }
-//      }
-//      case _ => Left(SeeOther("/signin"))
-//    }
-//  }
-//
-//  def getSC_GU_UCookie(cookies: Cookies): Option[Cookie] = cookies.get(CookieName.SC_GU_U.toString)
-//}

0c5edeece7b078067502c2e4f6600bf0180d1a04
Susie Coleman <susie.coleman@guardian.co.uk>
2016-03-14 17:25:45 +0000

    Ensure that failure to authenticate on a cookie redirects to sign in with the appropriate query params.

--- app/com/gu/identity/frontend/authentication/UserAuthenticatedAction.scala
+++ app/com/gu/identity/frontend/authentication/UserAuthenticatedAction.scala
@@ -1,14 +1,18 @@
 package com.gu.identity.frontend.authentication
 
+import java.net.URI
+
 import com.gu.identity.cookie.IdentityCookieDecoder
 import com.gu.identity.frontend.logging.Logging
-import controllers.routes
+import com.gu.identity.frontend.models.{ClientID, GroupCode}
+import com.gu.identity.frontend.controllers._
 import play.api.mvc._
 import play.api.mvc.Results.SeeOther
 
 
 
 import scala.concurrent.Future
+import scala.util.{Success, Try}
 
 class UserAuthenticatedRequest[A](val scGuUCookie: Cookie, request: Request[A]) extends WrappedRequest[A](request)
 
@@ -16,20 +20,32 @@ object UserAuthenticatedActionBuilder extends Logging{
   def UserAuthenticatedAction(identityCookieDecoder: IdentityCookieDecoder) = new ActionRefiner[Request, UserAuthenticatedRequest] with ActionBuilder[UserAuthenticatedRequest] {
     def refine[A](request: Request[A]): Future[Either[Result, UserAuthenticatedRequest[A]]] = Future.successful {
 
+      val returnUrl = request.getQueryString("returnUrl")
+      val skipConfirmation = request.getQueryString("skipConfirmation").map(_.toBoolean)
+      val clientId = ClientID(request.getQueryString("clientId"))
+      val groupCode = getGroupCode(request.uri)
+
       AuthenticationService.authenticatedUserFor(request, identityCookieDecoder.getUserDataForScGuU) match {
         case Some(authenticatedUser) => {
           getSC_GU_UCookie(request.cookies) match {
             case Some(cookie) => Right(new UserAuthenticatedRequest[A](cookie, request))
             case _ => {
               logger.error("Cookie not found on successfully authenticated request.")
-              Left(SeeOther("/signin"))
+              Left(SeeOther(routes.Application.signIn(Seq.empty, returnUrl, skipConfirmation, clientId.map(_.id), groupCode.map(_.getCodeValue)).url))
             }
           }
         }
-        case _ => Left(SeeOther("/signin"))
+        case _ => Left(SeeOther(routes.Application.signIn(Seq.empty, returnUrl, skipConfirmation, clientId.map(_.id), groupCode.map(_.getCodeValue)).url))
       }
     }
   }
   def getSC_GU_UCookie(cookies: Cookies): Option[Cookie] = cookies.get(CookieName.SC_GU_U.toString)
+
+  def getGroupCode(url: String): Option[GroupCode] = {
+    Try(new URI(url)) match {
+      case Success(uri) => GroupCode(uri.getPath.split("/").last)
+      case _ => None
+    }
+  }
 }
 

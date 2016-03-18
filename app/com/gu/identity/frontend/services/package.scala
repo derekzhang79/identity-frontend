58708009dd372542cfad7f89b2a2920fdce784e2
james@jamespamplin.com <james@jamespamplin.com>
2016-03-18 12:23:59 +0000

    Removed legacy error definitions

--- app/com/gu/identity/frontend/services/package.scala
+++ app/com/gu/identity/frontend/services/package.scala
@@ -7,19 +7,4 @@ package object services {
   type ServiceException = AppException
   type ServiceExceptions = Seq[AppException]
 
-//  type ServiceErrors = Seq[ServiceError]
-
-//  sealed trait ServiceError extends NoStackTrace {
-//    val id: String
-//    val message: String
-//    val description: Option[String]
-//  }
-
-//  case class ServiceGatewayError(message: String, description: Option[String] = None) extends ServiceError {
-//    override val id: String = "error-gateway"
-//  }
-//  case class ServiceBadRequest(message: String, description: Option[String] = None) extends ServiceError {
-//    override val id: String = "error-bad-request"
-//  }
-
 }

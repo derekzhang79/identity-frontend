b728f6670504f83d762b65a3b58c70b3a21fef6d
Fabio Crisci <fabio.crisci@guardian.co.uk>
2016-08-30 13:31:24 +0100

    convert two inputs in a single international phone field

--- public/components/lib/phone-field.js
+++ public/components/lib/phone-field.js
@@ -1,26 +1,21 @@
-export function initPhoneField (form, domElem) {
+const PLUGIN_OPTIONS = {
+  initialCountry: 'gb',
+  preferredCountries: ['gb', 'us', 'au']
+};
 
-  const PLUGIN_OPTIONS = {
-    nationalMode: false,
-    initialCountry: 'gb',
-    preferredCountries: ['gb', 'us', 'au']
-  };
+export function initPhoneField (form, countryCodeElement, localNumberElement) {
 
   require( [ 'jquery', 'intl-tel-input', 'intl-tel-input/build/js/utils.js' ], ($) => {
 
     const tooltipElement = form.formElement.elem.querySelector('.register-form__tooltip--phone-number');
-    $(domElem.elem).intlTelInput(PLUGIN_OPTIONS);
-    attachListeners(form.formElement.elem);
-
-    function attachListeners (formElem) {
-      formElem.addEventListener('click', setPhoneNumber);
-      formElem.querySelector('.register-form__link--why-phone-number').addEventListener('click', toggleDropdown);
-      formElem.querySelector('.register-form__tooltip--phone-number__close').addEventListener('click', toggleDropdown);
-    }
-
-    function setPhoneNumber () {
-      domElem.setValue($(domElem.elem).intlTelInput('getNumber'));
-    }
+    const formElement = form.formElement.elem;
+    initializeFields(
+      $(formElement),
+      $(countryCodeElement.elem),
+      $(localNumberElement.elem)
+    );
+    $('.register-form__link--why-phone-number', formElement).on('click', toggleDropdown);
+    $('.register-form__tooltip--phone-number__close', formElement).on('click', toggleDropdown);
 
     function toggleDropdown () {
       if (tooltipElement.hasAttribute('hidden')) {
@@ -30,4 +25,22 @@ export function initPhoneField (form, domElem) {
       }
     }
   });
+}
+
+function initializeFields (form, countryCode, localNumber) {
+  // The core view has a select and an input field. When JS is enabled and running
+  // hide the select and replace it with a jQuery phone number plugin
+  countryCode.parent().hide();
+  localNumber.parents('.register-form__control-column--local-number')
+    .removeClass('register-form__control-column--local-number')
+    .addClass('register-form__control-column--local-number--wide')
+  localNumber.intlTelInput(PLUGIN_OPTIONS);
+
+  form.on('submit', updateHiddenField);
+
+  function updateHiddenField () {
+    const dialCode = localNumber.intlTelInput('getSelectedCountryData').dialCode;
+    countryCode.val(dialCode);
+    localNumber.val(localNumber.intlTelInput('getNumber').replace(new RegExp('^\\+' + dialCode), ''));
+  }
 }
--- public/components/register-form/register-form.js
+++ public/components/register-form/register-form.js
@@ -9,21 +9,25 @@ const STORAGE_KEY = 'gu_id_register_state';
 
 
 class RegisterFormFields {
-  constructor(firstNameField, lastNameField, emailField, usernameField, optionalPhoneNumber) {
+  constructor(firstNameField, lastNameField, emailField, usernameField, optionalCountryCode, optionalPhoneNumber) {
     this.firstName = firstNameField;
     this.lastName = lastNameField;
     this.email = emailField;
     this.username = usernameField;
+    this.optionalCountryCode = optionalCountryCode;
     this.optionalPhoneNumber = optionalPhoneNumber;
   }
 
-  setValues( { firstName, lastName, email, username, optionalPhoneNumber } = {} ) {
+  setValues( { firstName, lastName, email, username, optionalCountryCode, optionalPhoneNumber } = {} ) {
     this.firstName.setValue( firstName );
     this.lastName.setValue( lastName );
     this.email.setValue( email );
     this.username.setValue( username );
     if (this.optionalPhoneNumber) {
       this.optionalPhoneNumber.setValue(optionalPhoneNumber);
+    }
+    if (this.optionalCountryCode) {
+      this.optionalCountryCode.setValue(optionalCountryCode);
     }
   }
 
@@ -38,10 +42,10 @@ class RegisterFormFields {
 
 
 class RegisterFormModel {
-  constructor( formElement, firstNameField, lastNameField, emailField, usernameField, optionalPhoneNumber ) {
+  constructor( formElement, firstNameField, lastNameField, emailField, usernameField, optionalCountryCode, optionalPhoneNumber ) {
     this.formElement = formElement;
 
-    this.fields = new RegisterFormFields( firstNameField, lastNameField, emailField, usernameField, optionalPhoneNumber );
+    this.fields = new RegisterFormFields( firstNameField, lastNameField, emailField, usernameField, optionalCountryCode, optionalPhoneNumber );
 
     this.addBindings();
   }
@@ -70,21 +74,23 @@ class RegisterFormModel {
     const lastNameField = getElementById( 'register_field_lastname' );
     const emailField = getElementById( 'register_field_email' );
     const usernameField = getElementById( 'register_field_username' );
-    const optionalPhoneNumber = getElementById('register_field_phone_number');
+    const optionalPhoneNumber = getElementById('register_field_localNumber');
+    const optionalCountryCode = getElementById('register_field_countryCode');
 
     if ( form && firstNameField && lastNameField && emailField && usernameField ) {
-      return new RegisterFormModel( form, firstNameField, lastNameField, emailField, usernameField, optionalPhoneNumber );
+      return new RegisterFormModel( form, firstNameField, lastNameField, emailField, usernameField, optionalCountryCode, optionalPhoneNumber );
     }
   }
 }
 
 
 class RegisterFormState {
-  constructor( firstName = "", lastName = "", email = "", username = "", optionalPhoneNumber = "" ) {
+  constructor( firstName = "", lastName = "", email = "", username = "", optionalCountryCode = "", optionalPhoneNumber = "" ) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.username = username;
+    this.optionalCountryCode = optionalCountryCode;
     this.optionalPhoneNumber = optionalPhoneNumber;
   }
 
@@ -95,8 +101,8 @@ class RegisterFormState {
   /**
    * @return {RegisterFormState}
    */
-  static fromObject( { firstName, lastName, email, username, optionalPhoneNumber } = {} ) {
-    return new RegisterFormState( firstName, lastName, email, username, optionalPhoneNumber );
+  static fromObject( { firstName, lastName, email, username, optionalCountryCode, optionalPhoneNumber } = {} ) {
+    return new RegisterFormState( firstName, lastName, email, username, optionalCountryCode, optionalPhoneNumber );
   }
 
   /**
@@ -112,7 +118,7 @@ class RegisterFormState {
    * @param {RegisterFormModel} form
    * @return {RegisterFormState}
    */
-  static fromForm( form ) {                       gbnf
+  static fromForm( form ) {
     return RegisterFormState.fromObject( form.fields.toJSON() );
   }
 }
@@ -124,11 +130,9 @@ export function init() {
   if ( form ) {
     form.loadState();
 
-/*
     if (form.fields.optionalPhoneNumber) {
-      initPhoneField(form, form.fields.optionalPhoneNumber);
+      initPhoneField(form, form.fields.optionalCountryCode, form.fields.optionalPhoneNumber);
     }
-*/
   }
 
   return form;

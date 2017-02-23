72c86dcece4a459d4d3a0b47cd0cbdaaeeb6ee85
Jon Soul <jonathan.soul@guardian.co.uk>
2017-02-23 09:28:39 +0000

    Initialise/terminate actorSystem only once - use new actor classes instead of traits

--- app/com/gu/identity/frontend/configuration/FrontendApplicationLoader.scala
+++ app/com/gu/identity/frontend/configuration/FrontendApplicationLoader.scala
@@ -1,22 +1,25 @@
 package com.gu.identity.frontend.configuration
 
-import com.gu.identity.cookie.{IdentityKeys, IdentityCookieDecoder}
+import akka.actor.{ActorRef, Props}
+import com.gu.identity.cookie.{IdentityCookieDecoder, IdentityKeys}
+import com.gu.identity.frontend.analytics.client.MeasurementProtocolClient
+import com.gu.identity.frontend.analytics.{AnalyticsEventActor, EventActor}
 import com.gu.identity.frontend.controllers._
 import com.gu.identity.frontend.csrf.CSRFConfig
 import com.gu.identity.frontend.errors.ErrorHandler
-import com.gu.identity.frontend.filters.{HtmlCompressorFilter, SecurityHeadersFilter, Filters}
-import com.gu.identity.frontend.logging.{SmallDataPointCloudwatchLogging, MetricsLoggingActor, SentryLogging}
-import com.gu.identity.frontend.services.{GoogleRecaptchaServiceHandler, IdentityServiceRequestHandler, IdentityServiceImpl, IdentityService}
+import com.gu.identity.frontend.filters.{Filters, HtmlCompressorFilter, SecurityHeadersFilter}
+import com.gu.identity.frontend.logging.{MetricsActor, MetricsLoggingActor, SentryLogging, SmallDataPointCloudwatchLogging}
+import com.gu.identity.frontend.services.{GoogleRecaptchaServiceHandler, IdentityService, IdentityServiceImpl, IdentityServiceRequestHandler}
 import com.gu.identity.service.client.IdentityClient
 import jp.co.bizreach.play2handlebars.HandlebarsPlugin
+import play.api.ApplicationLoader.Context
 import play.api.i18n.I18nComponents
+import play.api.libs.ws.ning.NingWSComponents
 import play.api.routing.Router
+import play.api.{ApplicationLoader, BuiltInComponentsFromContext, Logger, Mode}
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.filters.gzip.GzipFilter
 import router.Routes
-import play.api.libs.ws.ning.NingWSComponents
-import play.api.{Mode, Logger, BuiltInComponentsFromContext, ApplicationLoader}
-import play.api.ApplicationLoader.Context
-import play.api.libs.concurrent.Execution.defaultContext
 
 class FrontendApplicationLoader extends ApplicationLoader {
   def load(context: Context) = {
@@ -26,13 +29,20 @@ class FrontendApplicationLoader extends ApplicationLoader {
   }
 }
 
-class ApplicationComponents(context: Context) extends BuiltInComponentsFromContext(context) with NingWSComponents with I18nComponents with MetricsLoggingActor {
+class ApplicationComponents(context: Context) extends BuiltInComponentsFromContext(context) with NingWSComponents with I18nComponents {
   lazy val frontendConfiguration = Configuration(configuration)
   lazy val csrfConfig = CSRFConfig(configuration)
 
   lazy val identityServiceRequestHandler = new IdentityServiceRequestHandler(wsClient)
   lazy val identityClient: IdentityClient = new IdentityClient
   lazy val identityService: IdentityService = new IdentityServiceImpl(frontendConfiguration, identityServiceRequestHandler, identityClient)
+
+  lazy val measurementProtocolClient: MeasurementProtocolClient = new MeasurementProtocolClient(wsClient)
+  lazy val eventActor: ActorRef = actorSystem.actorOf(EventActor.getProps(measurementProtocolClient))
+  lazy val analyticsEventActor: AnalyticsEventActor = new AnalyticsEventActor(eventActor)
+
+  lazy val metricsActor: ActorRef = actorSystem.actorOf(Props[MetricsActor])
+  lazy val metricsLoggingActor: MetricsLoggingActor = new MetricsLoggingActor(metricsActor)
 
   lazy val identityCookieDecoder: IdentityCookieDecoder = new IdentityCookieDecoder(IdentityKeys(frontendConfiguration.identityCookiePublicKey))
 
@@ -43,9 +53,9 @@ class ApplicationComponents(context: Context) extends BuiltInComponentsFromConte
   lazy val cspReporterController = new CSPViolationReporter()
   lazy val googleRecaptchaServiceHandler = new GoogleRecaptchaServiceHandler(wsClient, frontendConfiguration)
   lazy val googleRecaptchaCheck = new GoogleRecaptchaCheck(googleRecaptchaServiceHandler)
-  lazy val signinController = new SigninAction(identityService, messagesApi, csrfConfig, frontendConfiguration)
+  lazy val signinController = new SigninAction(identityService, messagesApi, metricsLoggingActor, analyticsEventActor, csrfConfig, frontendConfiguration)
   lazy val signOutController = new SignOutAction(identityService, messagesApi, frontendConfiguration)
-  lazy val registerController = new RegisterAction(identityService, messagesApi, frontendConfiguration, csrfConfig)
+  lazy val registerController = new RegisterAction(identityService, messagesApi, metricsLoggingActor, analyticsEventActor, frontendConfiguration, csrfConfig)
   lazy val thirdPartyTsAndCsController = new ThirdPartyTsAndCs(identityService, frontendConfiguration, messagesApi, httpErrorHandler, identityCookieDecoder.getUserDataForScGuU)
   lazy val resetPasswordController = new ResetPasswordAction(identityService, csrfConfig)
   lazy val assets = new controllers.Assets(httpErrorHandler)
@@ -68,7 +78,12 @@ class ApplicationComponents(context: Context) extends BuiltInComponentsFromConte
     new SmallDataPointCloudwatchLogging(actorSystem).start
   }
 
-  applicationLifecycle.addStopHook(() => terminateActor()(defaultContext))
+  applicationLifecycle.addStopHook(() => {
+    metricsLoggingActor.terminateActor()
+    analyticsEventActor.terminateActor()
+    actorSystem.terminate().map(_ => ())
+  })
+
 
   override lazy val router: Router = new Routes(httpErrorHandler, applicationController, signOutController, thirdPartyTsAndCsController, signinController, registerController, resetPasswordController, cspReporterController, healthcheckController, digitalAssetLinksController, manifestController, assets, redirects)
 

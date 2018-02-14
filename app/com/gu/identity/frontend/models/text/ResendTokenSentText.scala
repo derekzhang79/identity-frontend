7a651e24f4f89b71ac17bd989762912c6b4fe23c
Calum Campbell <campbell.cj@hotmail.co.uk>
2018-02-14 11:54:39 +0000

    error headline on resnent page

--- app/com/gu/identity/frontend/models/text/ResendTokenSentText.scala
+++ app/com/gu/identity/frontend/models/text/ResendTokenSentText.scala
@@ -5,13 +5,15 @@ import play.api.i18n.Messages
 case class ResendTokenSentText private(
   pageTitle: String,
   title: String,
-  description: String)
+  description: String,
+  unexpectedTitle: String)
 
 object ResendTokenSentText {
   def apply()(implicit messages: Messages): ResendTokenSentText =
     ResendTokenSentText(
       pageTitle = messages("resendTokenSent.pageTitle"),
       title = messages("resendTokenSent.title"),
-      description = messages("resendTokenSent.description")
+      description = messages("resendTokenSent.description"),
+      unexpectedTitle = messages("resendTokenSent.unexpectedTitle")
     )
 }

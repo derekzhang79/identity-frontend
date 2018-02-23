b220fa02174f2a89c4145862dcb18c871ff4acfd
Laura <laura.gonzalez@guardian.co.uk>
2018-02-23 14:59:17 +0000

    link to /complete-registration

--- functional-tests/src/test/scala/pages/RegisterConfirm.scala
+++ functional-tests/src/test/scala/pages/RegisterConfirm.scala
@@ -5,9 +5,7 @@ import test.util.{LoadablePage, Browser, Config}
 class RegisterConfirm extends LoadablePage with Browser {
   val url = s"${Config.baseUrl}/register/confirm?returnUrl=${Config.baseUrl}/test-return-test"
 
-  def hasLoaded(): Boolean = pageHasUrl("verify-email")
-
-  def isSignupFlow(): Boolean = pageHasUrl("isSignupFlow")
+  def isCompleteRegistration(): Boolean = pageHasUrl("complete-registration")
 
   def hasPassedReturnUrl(): Boolean = pageHasUrl("test-return-test")
 

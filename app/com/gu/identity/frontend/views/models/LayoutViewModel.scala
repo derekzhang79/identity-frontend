4b178e46022822756e5a8a6ac82d77fbff712e9a
Laura <laura.gonzalez@guardian.co.uk>
2018-03-06 11:56:39 +0000

    cleaner diff

--- app/com/gu/identity/frontend/views/models/LayoutViewModel.scala
+++ app/com/gu/identity/frontend/views/models/LayoutViewModel.scala
@@ -25,7 +25,6 @@ case object BaseLayoutViewModel extends ViewModel with ViewModelResources {
     IndirectlyLoadedExternalScriptResources("https://j.ophan.co.uk"),
     IndirectlyLoadedExternalScriptResources("https://www.google-analytics.com"),
     IndirectlyLoadedExternalResources("https://app.getsentry.com/api/"),
-    IndirectlyLoadedExternalResources("*.thegulocal.com"),
     IndirectlyLoadedExternalImageResources("https://www.google-analytics.com"),
     IndirectlyLoadedExternalImageResources("https://hits-secure.theguardian.com"),
     IndirectlyLoadedExternalImageResources("https://sb.scorecardresearch.com"),

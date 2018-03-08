79de9457a7f988a89e52b6a84e931cd5b1312ff7
Laura <laura.gonzalez@guardian.co.uk>
2018-03-08 15:19:14 +0000

    prettier

--- public/components/two-step-signin/two-step-signin.js
+++ public/components/two-step-signin/two-step-signin.js
@@ -1,81 +1,90 @@
-import { EV_DONE, EV_START_OVER } from 'components/two-step-signin/two-step-signin__slide';
-import { loadComponents } from 'js/load-components';
-import { route } from 'js/config';
+import {
+  EV_DONE,
+  EV_START_OVER
+} from "components/two-step-signin/two-step-signin__slide";
+import { loadComponents } from "js/load-components";
+import { route } from "js/config";
 
-const className = 'two-step-signin';
-const slideClassName = 'two-step-signin__slide';
+const className = "two-step-signin";
+const slideClassName = "two-step-signin__slide";
 
-const ERR_MALFORMED_FETCH = 'Something went wrong';
-const STATE_INITIATOR = 'two-step-signin-state-init';
+const ERR_MALFORMED_FETCH = "Something went wrong";
+const STATE_INITIATOR = "two-step-signin-state-init";
 
-const getSlideFromFetch = (textHtml) => {
-  const $wrapper = document.createElement('div');
+const getSlideFromFetch = textHtml => {
+  const $wrapper = document.createElement("div");
   $wrapper.innerHTML = textHtml;
 
   const $form = $wrapper.querySelector(`.${slideClassName}`);
-  if($form !== null) return $form;
+  if ($form !== null) return $form;
   else throw new Error(ERR_MALFORMED_FETCH);
-}
+};
 
 const pushSlide = ($old, $new, reverse = false) => {
-
   const classNames = reverse
-    ? {in:'two-step-signin__slide--in-reverse',out:'two-step-signin__slide--out-reverse'}
-    : {in:'two-step-signin__slide--in',out:'two-step-signin__slide--out'};
+    ? {
+        in: "two-step-signin__slide--in-reverse",
+        out: "two-step-signin__slide--out-reverse"
+      }
+    : { in: "two-step-signin__slide--in", out: "two-step-signin__slide--out" };
 
-  return new Promise((resolve, error)=> {
-    $old.addEventListener('animationend', () => {
+  return new Promise((resolve, error) => {
+    $old.addEventListener("animationend", () => {
       $old.remove();
       resolve($new);
     });
     requestAnimationFrame(() => {
-      $old.classList.remove('two-step-signin__slide--visible');
+      $old.classList.remove("two-step-signin__slide--visible");
       $old.classList.add(classNames.out);
-    })
-    $new.addEventListener('animationend', () => {
+    });
+    $new.addEventListener("animationend", () => {
       [
-        'two-step-signin__slide--in',
-        'two-step-signin__slide--out',
-        'two-step-signin__slide--in-reverse',
-        'two-step-signin__slide--out-reverse'
+        "two-step-signin__slide--in",
+        "two-step-signin__slide--out",
+        "two-step-signin__slide--in-reverse",
+        "two-step-signin__slide--out-reverse"
       ].forEach(_ => $new.classList.remove(_));
     });
     requestAnimationFrame(() => {
-      ['two-step-signin__slide--visible', classNames.in].forEach(_ => $new.classList.add(_))
+      ["two-step-signin__slide--visible", classNames.in].forEach(_ =>
+        $new.classList.add(_)
+      );
     });
     $old.parentNode.appendChild($new);
   });
-}
-
+};
 
 const initOnce = () => {
-  window.addEventListener('popstate',(ev) => {
-    if(ev.state && ev.state.initiator && ev.state.initiator === STATE_INITIATOR) {
+  window.addEventListener("popstate", ev => {
+    if (
+      ev.state &&
+      ev.state.initiator &&
+      ev.state.initiator === STATE_INITIATOR
+    ) {
       ev.preventDefault();
       ev.stopPropagation();
       window.location.reload();
     }
   });
-}
+};
 
+const init = $component => {
+  $component.addEventListener(EV_DONE, ev => {
+    const $slide = $component.querySelector(`.${slideClassName}`);
 
-const init = ($component) => {
-
-  $component.addEventListener(EV_DONE, (ev) => {
-
-    const $slide = $component.querySelector(`.${slideClassName}`)
-
-    history.pushState({
-      initiator: STATE_INITIATOR
-    },'',ev.detail.url);
+    history.pushState(
+      {
+        initiator: STATE_INITIATOR
+      },
+      "",
+      ev.detail.url
+    );
 
     const $new = getSlideFromFetch(ev.detail.responseHtml);
-    pushSlide($slide, $new, ev.detail.reverse).then(()=>{
+    pushSlide($slide, $new, ev.detail.reverse).then(() => {
       loadComponents($new.parentNode);
     });
+  });
+};
 
-  })
-
-}
-
-export {init, className, initOnce}
+export { init, className, initOnce };
--- public/components/two-step-signin/two-step-signin__slide.js
+++ public/components/two-step-signin/two-step-signin__slide.js
@@ -1,73 +1,68 @@
-import { route } from 'js/config';
+import { route } from "js/config";
 
-const className = 'two-step-signin__slide';
+const className = "two-step-signin__slide";
 
-const SLIDE_STATE_READY = 'SLIDE_STATE_READY';
-const SLIDE_STATE_LOADING = 'SLIDE_STATE_LOADING';
+const SLIDE_STATE_READY = "SLIDE_STATE_READY";
+const SLIDE_STATE_LOADING = "SLIDE_STATE_LOADING";
 
-const EV_DONE = 'form-done';
+const EV_DONE = "form-done";
 
-const initStepOneForm = ($component,$parent) => {
-  $component.addEventListener('submit', (ev) => {
+const initStepOneForm = ($component, $parent) => {
+  $component.addEventListener("submit", ev => {
     ev.preventDefault();
     $component.dataset.state = SLIDE_STATE_LOADING;
 
-    fetch('/signin/existing',{
-      credentials: 'include',
+    fetch("/signin/existing", {
+      credentials: "include"
     })
-      .then(response =>
-        Promise.all([
-          response.text(),
-          response.url
-        ])
-      ).then(([text,url]) => {
-      const event = new CustomEvent(EV_DONE, {
-        bubbles: true,
-        detail: {
-          responseHtml: text,
-          url: url,
-        }
-      });
-      $parent.dispatchEvent(event);
-    })
-  })
-}
-
-const init = ($component) => {
-
-  console.log(`a[href*="${route('twoStepSignIn')}"]`);
-
-  const $form = $component.querySelector('form');
-  const $resetLinks = [...$component.querySelectorAll(`a[href*="${route('twoStepSignIn')}"]`)];
-
-  if( new URL($form.action).pathname === '/actions/signin/with-email') {
-    initStepOneForm($form,$component);
-  }
-
-  $resetLinks.forEach($resetLink=>{
-    $resetLink.addEventListener('click', ev => {
-      ev.preventDefault();
-
-      fetch(route('twoStepSignIn'), {
-          credentials: 'include',
-        }
-      ).then(response =>
-        Promise.all([
-          response.text(),
-          response.url
-        ])
-      ).then(([text, url]) =>
-        $component.dispatchEvent(new CustomEvent(EV_DONE, {
+      .then(response => Promise.all([response.text(), response.url]))
+      .then(([text, url]) => {
+        const event = new CustomEvent(EV_DONE, {
           bubbles: true,
           detail: {
             responseHtml: text,
-            url: url,
-            reverse: true
+            url: url
           }
-        }))
-      )
-    });
-  })
-}
+        });
+        $parent.dispatchEvent(event);
+      });
+  });
+};
 
-export {init, className, EV_DONE}
+const init = $component => {
+  console.log(`a[href*="${route("twoStepSignIn")}"]`);
+
+  const $form = $component.querySelector("form");
+  const $resetLinks = [
+    ...$component.querySelectorAll(`a[href*="${route("twoStepSignIn")}"]`)
+  ];
+
+  if (new URL($form.action).pathname === "/actions/signin/with-email") {
+    initStepOneForm($form, $component);
+  }
+
+  $resetLinks.forEach($resetLink => {
+    $resetLink.addEventListener("click", ev => {
+      ev.preventDefault();
+
+      fetch(route("twoStepSignIn"), {
+        credentials: "include"
+      })
+        .then(response => Promise.all([response.text(), response.url]))
+        .then(([text, url]) =>
+          $component.dispatchEvent(
+            new CustomEvent(EV_DONE, {
+              bubbles: true,
+              detail: {
+                responseHtml: text,
+                url: url,
+                reverse: true
+              }
+            })
+          )
+        );
+    });
+  });
+};
+
+export { init, className, EV_DONE };

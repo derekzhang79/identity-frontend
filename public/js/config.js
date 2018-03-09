add2b98c9fbaffc118d65a499b7a34d7a5217781
Laura <laura.gonzalez@guardian.co.uk>
2018-03-09 12:44:21 +0000

    lint

--- public/js/config.js
+++ public/js/config.js
@@ -1,24 +1,25 @@
 const ERR_MISSING_KEY = 'Missing configuration part';
 
-const config = {}
+const config = {};
 
 try {
   const configElem = document.getElementById('id_config').innerHTML;
   const parsed = JSON.parse(configElem);
-  Object.assign(config,parsed);
-} catch(err) {
-  console.warn(err)
-  throw(err);
+  Object.assign(config, parsed);
+} catch (err) {
+  console.warn(err);
+  throw err;
 }
 
 const get = key => {
-  if(config[key]) return config[key]
+  if (config[key]) return config[key];
   else throw new Error(ERR_MISSING_KEY);
-}
+};
 
 const route = route => {
-  if(config['routes'] && config['routes'][route]) return config['routes'][route];
+  if (config['routes'] && config['routes'][route])
+    return config['routes'][route];
   else throw new Error(ERR_MISSING_KEY);
-}
+};
 
-export {get, route}
+export { get, route };
--- public/js/load-components.js
+++ public/js/load-components.js
@@ -1,27 +1,34 @@
-import { init as initTwoStepSignin , className as classNameTwoStepSignin, initOnce as initOnceTwoStepSignin } from 'components/two-step-signin/two-step-signin.js';
-import { init as initTwoStepSigninSlide, className as classNameTwoStepSigninSlide } from 'components/two-step-signin/two-step-signin__slide.js';
+import {
+  init as initTwoStepSignin,
+  className as classNameTwoStepSignin,
+  initOnce as initOnceTwoStepSignin
+} from 'components/two-step-signin/two-step-signin.js';
+import {
+  init as initTwoStepSigninSlide,
+  className as classNameTwoStepSigninSlide
+} from 'components/two-step-signin/two-step-signin__slide.js';
 
 const components = [
   [initTwoStepSignin, classNameTwoStepSignin, initOnceTwoStepSignin],
-  [initTwoStepSigninSlide, classNameTwoStepSigninSlide],
-]
+  [initTwoStepSigninSlide, classNameTwoStepSigninSlide]
+];
 
 const initOnceList = [];
 
-const loadComponents = ($root) => {
+const loadComponents = $root => {
   components.forEach(component => {
     [...$root.querySelectorAll(`.${component[1]}`)]
       .filter($target => !$target.dataset.enhanced)
-      .forEach($target=>{
-        if(component[2] && !initOnceList.includes(component[1])) {
+      .forEach($target => {
+        if (component[2] && !initOnceList.includes(component[1])) {
           component[2]();
-          initOnceList.push(component[1])
+          initOnceList.push(component[1]);
         }
         $target.dataset.enhanced = true;
         component[0]($target);
-      })
-  })
-}
+      });
+  });
+};
 
-export {loadComponents}
+export { loadComponents };
 export default loadComponents;

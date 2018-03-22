8164fa96b008cc6e1d2a807f5538b8876e4c71dc
Laura <laura.gonzalez@guardian.co.uk>
2018-03-22 16:34:22 +0000

    use flow on config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,27 +1,32 @@
-const ERR_MISSING_KEY = 'Missing configuration part';
+// @flow
+
+const ERR_MISSING_KEY: string = 'Missing configuration part';
+const ERR_MISSING_CONFIG: string = 'Missing #id_config';
 
 const config = (() => {
   try {
-    const configElem = document.getElementById('id_config').innerHTML;
-    return JSON.parse(configElem);
+    const $idConfig = document.getElementById('id_config');
+    if (!$idConfig) throw new Error(ERR_MISSING_CONFIG);
+    const configText = $idConfig.innerHTML;
+    return JSON.parse(configText);
   } catch (err) {
     console.error(err);
     return {};
   }
 })();
 
-const get = key => {
+const get = (key: string) => {
   if (config[key]) return config[key];
   throw new Error(ERR_MISSING_KEY);
 };
 
-const route = routeToGet => {
+const route = (routeToGet: string) => {
   if (config.routes && config.routes[routeToGet])
     return config.routes[routeToGet];
   throw new Error(ERR_MISSING_KEY);
 };
 
-const localisedError = localisedErrorToGet => {
+const localisedError = (localisedErrorToGet: string) => {
   if (config.localisedErrors && config.localisedErrors[localisedErrorToGet])
     return config.localisedErrors[localisedErrorToGet];
   throw new Error(ERR_MISSING_KEY);

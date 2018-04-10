4690382397a1236b4d92e1c6e0f437fbfe27916c
Laura <laura.gonzalez@guardian.co.uk>
2018-04-10 16:29:53 +0100

    bugs

--- public/components/oauth-email/oauth-email.js
+++ public/components/oauth-email/oauth-email.js
@@ -5,21 +5,22 @@ const ERR_MALFORMED_HTML = 'Malformed html';
 const selector: string = '.oauth-email';
 
 const init = ($component: HTMLElement): void => {
+  const $button: ?HTMLElement = $component.querySelector('.oauth-email__cta');
+  const $wrap: ?HTMLElement = $component.querySelector('.oauth-email__wrap');
 
-  const $button: HTMLElement = $component.querySelector('.oauth-email__cta');
-  const $wrap: HTMLElement = $component.querySelector('.oauth-email__wrap');
-
-  if(!$button || !$wrap) {
+  if (!$button || !$wrap) {
     throw new Error(ERR_MALFORMED_HTML);
   }
 
+  const $focusable: ?HTMLElement = $wrap.querySelector('input[autofocus]');
+
   $wrap.style.display = 'none';
 
-  $button.addEventListener('click',(ev:Event)=>{
+  $button.addEventListener('click', (ev: Event) => {
+    ev.preventDefault();
     $wrap.style.display = 'block';
-  })
-
-
+    if ($focusable) $focusable.focus();
+  });
 };
 
 export { init, selector };
